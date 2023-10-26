8dd927af500fd84210e72b4ee45035b021c59275
haomingbai <haomingbai@hotmail.com>
2023-10-26 20:08:35 +0800

    202310262008

--- 3-004.cpp
+++ 3-004.cpp
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 /*我们这一个题目的要求是输入两个数，然后输出他们的乘法的竖式
@@ -22,3 +23,61 @@ using namespace std;
  * 最后，我们再像之前那样，画一条分割线
  * 输出结果，长度为运算结果
  * OK
+ * 所以，我们需要如下几个变量：
+ * 1，乘积的宽度
+ * 2，两个数字
+ * 3，乘积
+ * 4，第二个数字的宽度（可选）
+ * 5，第二个数字的各个数位的大小（临时）
+ * 最后，我们就能轻松完成任务*/
+
+//我们先编写一个函数，这个函数可以获取一个数字的长度
+
+int get_width(int number){
+	int i=0; //i是我们的数字的位数
+	while(number!=0){
+		number/=10;
+		i++;
+	}
+	return i;
+}
+
+//我们再编写一个函数，让这个函数可以自动地输出一个数字，并且不用打一长串的cout
+
+void ot(int output,int w){
+	cout<<setw(w)<<output<<endl;
+}
+
+int main(){
+	int factor1,factor2,product,product_width,factor2_width,factor2_part;
+	cin>>factor1>>factor2;
+	product=factor1*factor2;
+	product_width=get_width(product);
+	factor2_width=get_width(factor2);
+	ot(factor1,product_width+1);
+	cout<<'x';
+	ot(factor2,product_width);
+	for(int i=1;i<=product_width+1;i++){
+		cout<<'-';
+	}
+	printf("\n");
+
+	for(int i=1;i<=factor2_width;i++){
+		factor2_part=factor2%10;
+		if(i==factor2_width){
+			printf("+");
+		}else{
+			cout<<" ";
+		}
+		ot(factor2_part*factor1,product_width+1-i);
+		factor2/=10;
+	}
+
+	for(int i=1;i<=product_width+1;i++){
+		cout<<'-';
+	}
+	printf("\n");
+	ot(product,product_width+1);
+	return 0;
+
+}
